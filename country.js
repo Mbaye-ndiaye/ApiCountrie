212831fcbe60032db81bb20f378fd25984903682
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-10-29 00:48:12 +0000

    Apicountry:

--- country.js
+++ country.js
@@ -1,36 +1,29 @@
 
-
 const affiche = document.getElementById('region')
-// const clikable = document.getElementById('click')
 const input = document.getElementById('myInput')
 const bouton = document.getElementById('dark')
 const select = document.getElementById('countrie')
-const error = document.getElementById('error');
-// console.log(select);
+ let filterCountries = []
 
-// console.log(input)
 function afficheDesPays(pays) {
     affiche.innerHTML = '';
 
-    pays.forEach(country => {
+    pays.forEach(villes => {
         const div = document.createElement('div')
-        
         div.innerHTML = `
         <div class="country">
-        <img src = "${country.flags.png}"><br>
-        <h4> ${country.name.common}<h4><br>
-        <span> region: ${country.region}</span><br>
-        <span> population: ${country.population}</span><br>
-        <span> Capital: ${country.capital}</span><br>`
-
+        <img src = "${villes.flags.png}"><br>
+        <h4> ${villes.name.common}<h4><br>
+        <span> region: ${villes.region}</span><br>
+        <span> population: ${villes.population}</span><br>
+        <span> Capital: ${villes.capital}</span><br>`
         affiche.appendChild(div)
     })
 bouton.addEventListener('click', function(){
     document.body.classList.toggle('color')
-    
     bouton.classList.toggle('color')
 })
-    
+   
 }
 fetch('https://restcountries.com/v3.1/all')
 .then(response => response.json())
@@ -38,74 +31,67 @@ fetch('https://restcountries.com/v3.1/all')
         afficheDesPays(r)
 
             input.addEventListener('input', function() {
-                if(input.value == '') {
-                    
+                if(input.value == '') {   
                 } else {
                     fetch(`https://restcountries.com/v3.1/name/${input.value}`)
                     .then(pays => pays.json())
                     .then(data => {
 
-             afficheDesPays(data)    
-
+                         filterCountries = data
+                        // console.log({filterCountries});
+                        afficheDesPays(filterCountries)
                     })   
 
                 }
             })
 
            select.addEventListener('change', function(e){
-            console.log(select.value);
+            // console.log(e.currentTarget.value);
             fetch(`https://restcountries.com/v3.1/region/${select.value}`)
            
             .then(pays => pays.json())
             .then(region => { 
-                afficheDesPays(region)
-           
+
+                // console.log();
+              
+                    
+                
+             
+                    afficheDesPays(region)
            }) 
-        })
 
-
-
-        select.addEventListener('change', function() {
-            // affiche.innerHTML = '';
-            const input = select.value;
-            const paysTrouves = afficheDesPays(input);
-
-            if(paysTrouves.length > 0) {
-                afficheDesPays(paysTrouves)
-                error.style.display = 'none';
-            }else {
-                error.style.display = 'block'
-            }
-        })
-
-        
-
-
-
-
-        
-       
-
-    
-          })
-       
-
-            
-            
-            
-            
-           
+ 
           
+        })
 
 
-    
+   
+       
+    function paysParContinent() {
+        const selectContinent = document.querySelector(".form-select");
+        const countrySearch = document.querySelector(".search-country");
+        // console.log(countrySearch);
+      
+        selectContinent.addEventListener("change", updateDisplayedCountries);
+        countrySearch.addEventListener("input", updateDisplayedCountries);
+      
+        function updateDisplayedCountries() {
+          const selectedContinent = selectContinent.value.toLowerCase();
+          const searchTerm = countrySearch.value.trim().toLowerCase();
+          const filteredCountrie = filterCountries.filter((pays) => {
+            //   console.log(filteredCountrie, 'ok');
+            return (
+              (selectedContinent === "all" ||
+                pays.region.toLowerCase() === selectedContinent) &&
+              pays.name.common.toLowerCase().includes(searchTerm)
+            );
+          });
+          afficheDesPays(filteredCountrie);
+        }
+      }
+      paysParContinent();
+      
 
-        // }
-
-        //    ) 
-
-        
-
-
+    })
 
 
